b104a9c7df461e350e60bfb7b83040dd59fd6eb5
Jacob Rammer <jrammer101@gmail.com>
2025-06-08 18:56:20 -0700

    Revert "Revert "Updated app layout to use Mantine appshell""

--- client-app/src/components/accounts/accountDashboard.tsx
+++ client-app/src/components/accounts/accountDashboard.tsx
@@ -1,9 +1,8 @@
 import {observer} from "mobx-react-lite";
 import {useStore} from "../../stores/store.ts";
 import Sidebar from "../sidebar/sidebar.tsx";
-import {Box, Flex, MantineProvider, Modal, Text, Title, Tooltip} from "@mantine/core";
-import {useEffect} from "react";
-import {CircleHelp, CirclePlus, Smile} from "lucide-react";
+import {AppShell, Box, Flex, MantineProvider, Modal, Skeleton, Text, Title, Tooltip} from "@mantine/core";
+import {CircleHelp, CirclePlus, Group, Smile} from "lucide-react";
 import AccountTable from "./accountTable.tsx";
 import {useDisclosure} from "@mantine/hooks";
 import CreateAccountForm from "./createAccountForm.tsx";
@@ -19,11 +18,14 @@ export default observer(function AccountDashboard() {
     
     return (
         <MantineProvider >
-            <Flex>
-                <Sidebar/>
-                <Box className='DetailsPage'>
-                    <Box className='DetailHeaderWithUnderline'>
-                        <Box className='DetailHeaderDetailsWithMargin'>
+
+            <AppShell
+                layout="alt"
+                navbar={{ width: 250, breakpoint: 'sm'}}
+                header={{ height: 100 }}
+                padding='md'
+            >
+                <AppShell.Header p='xs'>
                             <Title order={2}>Account Dashboard</Title>
                             <Flex align='center'>
                                 <Text>Available cash: ${cashBalance}</Text>
@@ -32,34 +34,35 @@ export default observer(function AccountDashboard() {
                                     multiline
                                     w={220}
                                 >
-                                    <CircleHelp size={15} style={{marginLeft: '10px'}}/>
+                                    <CircleHelp size={15} style={{ marginLeft: '10px' }} />
                                 </Tooltip>
                             </Flex>
 
                             <Flex align='center'>
                                 <Text>Total balance: ${totalBalance()}</Text>
                                 <Tooltip label={<Text>This number is often times depressing. This is your net worth (available cash
-                                    - debts). I hated to see this number too the first time I added up my debts. I believe in you! <Smile size={15} style={{marginBottom: '-2px'}}/></Text>}
+                                    - debts). I hated to see this number too the first time I added up my debts. I believe in you! <Smile size={15} style={{ marginBottom: '-2px' }} /></Text>}
                                     multiline
                                     w={220}
                                 >
-                                    <CircleHelp size={15} style={{marginLeft: '10px'}}/>
+                                    <CircleHelp size={15} style={{ marginLeft: '10px' }} />
                                 </Tooltip>
                             </Flex>
-                        </Box>
-                    </Box>
-
+                </AppShell.Header>
+                <AppShell.Navbar>
+                    <Sidebar/>
+                </AppShell.Navbar>
+                <AppShell.Main>
                     <AccountTable/>
                     <Tooltip label="Add account" position="top-start">
-                        <CirclePlus size={40} className="AddActionCircle" onClick={open}/>
+                        <CirclePlus size={40} className="AddActionCircle" onClick={open} />
                     </Tooltip>
 
                     <Modal opened={opened} onClose={close} title="Add Account" centered closeOnClickOutside={false}>
-                        <CreateAccountForm onCloseModal={close}/>
+                        <CreateAccountForm onCloseModal={close} />
                     </Modal>
-                </Box>
-                
-            </Flex>
+                </AppShell.Main>
+            </AppShell>
             
         </MantineProvider>
             
