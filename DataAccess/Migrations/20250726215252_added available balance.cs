6b272942d47b3d8fae0922e88497bd842e6d0c78
Jacob Rammer <jrammer101@gmail.com>
2025-07-26 16:10:26 -0700

    god damned rider revered all modified files on apply patch. Like wtf

---
+++ DataAccess/Migrations/20250726215252_added available balance.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedavailablebalance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Available",
+                table: "Accounts",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Available",
+                table: "Accounts");
+        }
+    }
+}
