fb35aa19aa0bca1e4b1e3e23bc0ebfacb5c6236d
Jacob Rammer <jrammer101@gmail.com>
2025-08-06 20:55:37 -0700

    BudgetItems can now be deselected

--- client-app/src/components/dashboard/budgetItems/selectedBudgetItemInfo.tsx
+++ client-app/src/components/dashboard/budgetItems/selectedBudgetItemInfo.tsx
@@ -1,14 +1,16 @@
-import { Box } from "@mantine/core";
-import { observer } from "mobx-react-lite";
+import {Box} from "@mantine/core";
+import {observer} from "mobx-react-lite";
 import SelectedBudgetItemFundingOptions from "./selectedBudgetItemFundingOptions";
 import SelectedBudgetItemSpendingInfo from "./selectedBudgetItemSpendingInfo";
 import SelectedBudgetItemFundingInfo from "./selectedBudgetItem/selectedBudgetItemFundingInfo.tsx";
 import DeleteSelectedBudgetItem from "./buttons/deleteSelectedBudgetItem.tsx";
+import DeselectBudgetItemButton from "./buttons/deselectBudgetItemButton.tsx";
 
 export default observer(function SelectedBudgetItemInfo() {
 
     return (
         <Box>
+            <DeselectBudgetItemButton/>        
             <SelectedBudgetItemSpendingInfo />
             <SelectedBudgetItemFundingInfo />
             <SelectedBudgetItemFundingOptions />
